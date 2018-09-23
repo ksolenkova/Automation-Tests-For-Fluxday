6ea76909ef56f80ab951ce9816ae83440e12516b
ksolenkova <35841268+ksolenkova@users.noreply.github.com>
2018-09-23 22:08:10 +0300

    Add files via upload

---
+++ GitRepoProject/README.md
@@ -0,0 +1,18 @@
+# C# Selenium Webdriver Framework
+## ITAcademy 2018
+
+### Get started with the project:
+1. Select desired branch 
+- master branch should have the last stable version
+- development branch should have the latest version with latest progress at class
+- e.g. version-001 branch should have the first version and so on.
+2. Download the branch
+3. Open, build the project and download dependent libraries with NuGet package. Check if tests pass.
+4. Create your own repository
+5. Make an initial commit
+6. Put the downloaded code to your repository
+7. Make another commit
+8. Go on with writing your own tests
+
+## If you have questions
+Skype: MlaDANCe
---
+++ GitRepoProject/SeleniumWebDriver/AutomationTestsForFluxday.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace AutomationTestsForFluxday
+{
+    [TestClass]
+    public class AutomationTestsForFluxday
+    {
+        IWebDriver driver;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://app.fluxday.io/users/sign_in");
+        }
+
+        [TestCleanup]
+        public void TestTeardown()
+        {
+            driver.Quit();
+        }
+
+        [TestCategory("AdminsTests")]
+        [TestMethod]
+        public void Test001LogInAsAnAdmin()
+        {
+            IWebElement emailField = driver.FindElement(By.Id("user_email"));
+            emailField.SendKeys("admin@fluxday.io");
+
+            IWebElement passwordField = driver.FindElement(By.Id("user_password"));
+            passwordField.SendKeys("password");
+
+            IWebElement loginButton = driver.FindElement(By.ClassName("btn-login"));
+            loginButton.Click();
+
+            var fadeOutMessage = driver.FindElement(By.XPath("/html/body/div[2]/div[1]/div")).Text;
+
+            Assert.IsTrue(fadeOutMessage.Contains("Signed in successfully."));
+        }
+    }
+}
